7fa58d1c2bcdfeb3b218632ba77428a687a6a044
xinghuang-gao <47017982+xinghuang-gao@users.noreply.github.com>
2020-04-02 01:59:13 -0400

    Update polynorm.sklean.py

--- polynorm.sklean.py
+++ polynorm.sklean.py
@@ -1,60 +1,27 @@
-{
- "cells": [
-  {
-   "cell_type": "code",
-   "execution_count": null,
-   "metadata": {},
-   "outputs": [],
-   "source": [
-    "# TODO: Add import statements\n",
-    "from sklearn.linear_model import LinearRegression\n",
-    "from sklearn.preprocessing import PolynomialFeatures\n",
-    "import numpy as np\n",
-    "import pandas as pd\n",
-    "# Assign the data to predictor and outcome variables\n",
-    "# TODO: Load the data\n",
-    "train_data = pd.read_csv(\"data.csv\")\n",
-    "X = train_data['Var_X'].values.reshape(-1, 1)\n",
-    "y = train_data['Var_Y'].values\n",
-    "\n",
-    "\n",
-    "# Create polynomial features\n",
-    "# TODO: Create a PolynomialFeatures object, then fit and transform the\n",
-    "# predictor feature\n",
-    "poly_feat = PolynomialFeatures(degree = 4)\n",
-    "X_poly = poly_feat.fit_transform(X)\n",
-    "\n",
-    "# Make and fit the polynomial regression model\n",
-    "# TODO: Create a LinearRegression object and fit it to the polynomial predictor\n",
-    "# features\n",
-    "poly_model = LinearRegression()\n",
-    "poly_model.fit(X_poly, y)\n",
-    "\n",
-    "# Once you've completed all of the steps, select Test Run to see your model\n",
-    "# predictions against the data, or select Submit Answer to check if the degree\n",
-    "# of the polynomial features is the same as ours!"
-   ]
-  }
- ],
- "metadata": {
-  "kernelspec": {
-   "display_name": "Python 3",
-   "language": "python",
-   "name": "python3"
-  },
-  "language_info": {
-   "codemirror_mode": {
-    "name": "ipython",
-    "version": 3
-   },
-   "file_extension": ".py",
-   "mimetype": "text/x-python",
-   "name": "python",
-   "nbconvert_exporter": "python",
-   "pygments_lexer": "ipython3",
-   "version": "3.7.4"
-  }
- },
- "nbformat": 4,
- "nbformat_minor": 2
-}
+# TODO: Add import statements
+from sklearn.linear_model import LinearRegression
+from sklearn.preprocessing import PolynomialFeatures
+import numpy as np
+import pandas as pd
+# Assign the data to predictor and outcome variables
+# TODO: Load the data
+train_data = pd.read_csv("data.csv")
+X = train_data['Var_X'].values.reshape(-1, 1)
+y = train_data['Var_Y'].values
+
+
+# Create polynomial features
+# TODO: Create a PolynomialFeatures object, then fit and transform the
+# predictor feature
+poly_feat = PolynomialFeatures(degree = 4)
+X_poly = poly_feat.fit_transform(X)
+
+# Make and fit the polynomial regression model
+# TODO: Create a LinearRegression object and fit it to the polynomial predictor
+# features
+poly_model = LinearRegression()
+poly_model.fit(X_poly, y)
+
+# Once you've completed all of the steps, select Test Run to see your model
+# predictions against the data, or select Submit Answer to check if the degree
+# of the polynomial features is the same as ours!
