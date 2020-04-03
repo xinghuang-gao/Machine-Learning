730024c39654062e0fe0c6914226bbf2fdc467f3
xinghuang-gao <47017982+xinghuang-gao@users.noreply.github.com>
2020-04-03 00:42:52 -0400

    Update polynorm.sklean.py

--- polynorm.sklean.py
+++ polynorm.sklean.py
@@ -1,4 +1,26 @@
-# TODO: Add import statements
+########## Mutiple linear regression
+from sklearn.linear_model import LinearRegression
+from sklearn.datasets import load_boston
+
+# Load the data from the boston house-prices dataset 
+boston_data = load_boston()
+x = boston_data['data']
+y = boston_data['target']
+
+# Make and fit the linear regression model
+# TODO: Fit the model and assign it to the model variable
+model = LinearRegression()
+model = model.fit(x, y)
+
+# Make a prediction using the model
+sample_house = [[2.29690000e-01, 0.00000000e+00, 1.05900000e+01, 0.00000000e+00, 4.89000000e-01,
+                6.32600000e+00, 5.25000000e+01, 4.35490000e+00, 4.00000000e+00, 2.77000000e+02,
+                1.86000000e+01, 3.94870000e+02, 1.09700000e+01]]
+# TODO: Predict housing price for the sample_house
+prediction = model.predict(sample_house)
+
+
+########## Polynomination regression
 from sklearn.linear_model import LinearRegression
 from sklearn.preprocessing import PolynomialFeatures
 import numpy as np
@@ -25,3 +47,26 @@ poly_model.fit(X_poly, y)
 # Once you've completed all of the steps, select Test Run to see your model
 # predictions against the data, or select Submit Answer to check if the degree
 # of the polynomial features is the same as ours!
+
+################### Lasso regularization
+# TODO: Add import statements
+from sklearn import linear_model
+import numpy as np
+import pandas as pd
+
+# Assign the data to predictor and outcome variables
+# TODO: Load the data
+train_data = pd.read_csv("data.csv", header = None)
+X = train_data.iloc[:,0:6].values
+y = train_data.iloc[:,-1].values
+
+# TODO: Create the linear regression model with lasso regularization.
+lasso_reg = linear_model.Lasso()
+
+
+# TODO: Fit the model.
+lasso_reg.fit(X, y)
+
+# TODO: Retrieve and print out the coefficients from the regression model.
+reg_coef = lasso_reg.coef_
+print(reg_coef)
